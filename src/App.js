1408488ee525497e7fc6ccd2030d24096cc6f24f
Marla Glasser <mbglasser@gmail.com>
2019-03-22 18:59:41 -0400

    MVP! Use createRef to get new center and bounds. Add shouldFetch and didUpdate.

--- src/App.js
+++ src/App.js
@@ -12,20 +12,30 @@ class App extends Component {
       lg: -79,
       ht: 0.75,
       width: 1.5,
-      loading: true,
-      data: [],
       zoom: 10,
+      data: [],
+      loading: true,
+      shouldFetch: false,
     };
     this.getData = this.getData.bind(this);
     this.handleMoveZoom = this.handleMoveZoom.bind(this);
   }
 
+  mapRef = createRef();
+
   componentDidMount() {
-    const { lat, lg, ht, width } = this.state;
-    this.getData(lat, lg, ht, width);
+    this.getData();
   }
 
-  getData(lat, lg, ht, width) {
+  componentDidUpdate() {
+    if (this.state.shouldFetch) {
+      this.getData();
+      this.setState({ shouldFetch: false });
+    }
+  }
+
+  getData() {
+    const { lat, lg, ht, width } = this.state;
     fetch(`https://geodata-python-api.herokuapp.com/?lat=${lat}&lg=${lg}&ht=${ht}&width=${width}`)
       .then(res => res.json())
       .then(data => {
@@ -35,33 +45,40 @@ class App extends Component {
       .catch(err => console.error(err));
   }
 
-  handleMoveZoom(e) {
-    const zoomLevel = e.target._zoom;
-    const [lat, lg] = e.target.options.center;
-    const ht = 180 / (2 ** zoomLevel);
-    const width = ht * 2;
+  handleMoveZoom() {
+    const map = this.mapRef.current.leafletElement;
+    const zoom = map.getZoom();
+    const center = map.getCenter();
+    const bounds = map.getBounds();
+    const lat = center.lat;
+    const lg = center.lng;
+    const ht = Math.abs(bounds._northEast.lat - bounds._southWest.lat) * 2;
+    const width = Math.abs(bounds._northEast.lng - bounds._southWest.lng) * 2;
     this.setState({
       lat,
       lg,
       ht,
       width,
-      zoom: zoomLevel,
+      zoom,
+      shouldFetch: true,
       loading: true,
     });
-    this.getData(lat, lg, ht, width);
   }
 
   render() {
     const { lat, lg, loading, data, zoom } = this.state;
     return (
       <>
-      <p>{loading ? 
-        'Loading IP map...' :
-        `There are ${data.length} IP addresses in this region.`
-      }</p>
+      <p>
+        {loading ? 
+          'Loading IP map...' :
+          `There are ${data.length} IP addresses in this region.`
+        }
+      </p>
       <Map
         center={[lat, lg]} 
         zoom={zoom}
+        ref={this.mapRef}
         onMovestart={this.handleMoveZoom}
       >
         <HeatmapLayer
@@ -72,6 +89,7 @@ class App extends Component {
         />
         <TileLayer
           url={`http://{s}.tile.osm.org/{z}/{x}/{y}.png`}
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         />
       </Map>
       </>
