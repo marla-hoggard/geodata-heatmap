6a96c98db05253a983c2ace871850736e55ace5c
Marla Glasser <mbglasser@gmail.com>
2019-03-22 16:11:54 -0400

    Initial setup. create-react-app. Test api fetch in App.

--- src/App.js
+++ src/App.js
@@ -1,27 +1,53 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
+
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      lat: 36,
+      lg: -79,
+      ht: 10,
+      width: 20,
+      loading: true,
+      data: [],
+    };
+  }
+  componentDidMount() {
+    console.log('mounted');
+    const { lat, lg, ht, width } = this.state;
+    fetch(`https://geodata-python-api.herokuapp.com/?lat=${lat}&lg=${lg}&ht=${ht}&width=${width}`)
+      .then(res => res.json())
+      .then(data => {
+        console.log(data);
+        this.setState({ data: [...data], loading: false });
+      })
+      .catch(err => console.error(err));
+  }
+
   render() {
+    const position = [36, 42];
+    const zoom = 7;
+    if (this.state.loading) {
+      return <p>Loading...</p>;
+    }
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+      <>
+      <p>{this.state.data.length}</p>
+      <Map center={position} zoom={zoom}>
+        <TileLayer
+          url={`https://tile.openstreetmap.org/${zoom}/${position[0]}/${position[1]}.png`}
+        />
+        <Marker position={position}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </Map>
+      </>
+    )
   }
 }
 
